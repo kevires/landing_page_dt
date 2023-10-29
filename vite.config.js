55393bc3cf0dd6d5ac7fb4243004c37b1a19f302
khaiNh <khainguyenhuu3101@gmail.com>
2023-10-30 00:50:48 +0700

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const { createVuePlugin } = require('vite-plugin-vue2')
 dns.setDefaultResultOrder('verbatim')
 
 module.exports = {
+  base: '/landing_page_dt/',
   plugins: [createVuePlugin(), viteSingleFile()],
   externals: ['vue'],
   server: {
