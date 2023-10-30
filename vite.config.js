86752f6ef15a1a994adfb235ea9d888090bc6da6
khaiNh <khainguyenhuu3101@gmail.com>
2023-10-30 22:23:18 +0700

    Modify yaml

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ dns.setDefaultResultOrder("verbatim");
 import path from "path";
 
 module.exports = {
-  base:'',
+  base:'landing_page_dt',
   plugins: [createVuePlugin()],
   externals: ["vue"],
   server: {
